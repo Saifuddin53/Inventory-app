3206b51fdfe67198a614abd273ad2d63e06853b7
Saifuddin <adenwala32@gmail.com>
2024-02-26 19:53:39 +0530

    ItemDao created

---
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,28 @@
+package com.example.inventory.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface ItemDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("Select * from items where id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Query("Select * from items ORDER BY name ASC")
+    fun getAllItem(item: Item): Flow<List<Item>>
+}
